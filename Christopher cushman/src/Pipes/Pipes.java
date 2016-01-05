c9c15674c7a19a4f0a092c661c90b5e8352ab83f
17cc0151 <17cc0151@HSS-101-03.westwindsor.k12.nj.us>
2016-01-05 13:38:31 -0500

    finished

--- Christopher cushman/src/Pipes/Pipes.java
+++ Christopher cushman/src/Pipes/Pipes.java
@@ -13,23 +13,25 @@ public class Pipes {
 	public static void main(String[] args) throws InterruptedException {
 		JFrame frmMain = new JFrame();
 		frmMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmMain.setSize(400, 400);
+		frmMain.setSize(800, 800);
 
 		Canvas cnvs = new Canvas();
-		cnvs.setSize(400, 400);
+		cnvs.setSize(800, 800);
 
 		frmMain.add(cnvs);
 		frmMain.setVisible(true);
 
 		Graphics g = cnvs.getGraphics();
+		while(true) {
 		g.setColor(new Color(255, 0, 0));
-		int y = (int) (Math.random() * (401));
-		int x = (int) (Math.random() * (401));
+		int y = (int) (Math.random() * (801));
+		int x = (int) (Math.random() * (801));
 		Queue<Point> lines = new LinkedList<Point>();
 		
-		for (int ii = 0; ii <= 5; ii++) {
-			int a = (int) (Math.random() * (401));
-			int b = (int) (Math.random() * (401));
+		for (int ii = 0; ii <= 10; ii++) {
+			g.setColor(getRandomColor());
+			int a = (int) (Math.random() * (801));
+			int b = (int) (Math.random() * (801));
 			lines.offer(new Point(x, y));
 			lines.offer(new Point(a, b));
 			g.drawLine(x, y, a, b);
@@ -45,7 +47,14 @@ public class Pipes {
 			g.drawLine(beg.x, beg.y, end.x, end.y);
 			Thread.sleep(250);
 		}
+		}
 
+	}
+	
+	private static Color getRandomColor() {
+		Random randomNumber = new Random();
+	    return new Color(randomNumber.nextFloat(),
+	            randomNumber.nextFloat(), randomNumber.nextFloat());
 	}
 
 }
